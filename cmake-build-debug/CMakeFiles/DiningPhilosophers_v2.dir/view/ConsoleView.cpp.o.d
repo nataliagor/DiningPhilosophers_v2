0766f3dc397d8852cc039bfd2c17940b98050271
nataliagor <natalkagornikowska@gmail.com>
2023-03-25 21:31:57 +0100

    cos dziala v3

---
+++ cmake-build-debug/CMakeFiles/DiningPhilosophers_v2.dir/view/ConsoleView.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/DiningPhilosophers_v2.dir/view/ConsoleView.cpp.o: \
+ /home/natalia/CLionProjects/DiningPhilosophers_v2/view/ConsoleView.cpp \
+ /usr/include/stdc-predef.h \
+ /home/natalia/CLionProjects/DiningPhilosophers_v2/view/ConsoleView.h
---
+++ cmake-build-debug/CMakeFiles/DiningPhilosophers_v2.dir/view/TerminalView.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/DiningPhilosophers_v2.dir/view/TerminalView.cpp.o: \
+ /home/natalia/CLionProjects/DiningPhilosophers_v2/view/TerminalView.cpp \
+ /usr/include/stdc-predef.h \
+ /home/natalia/CLionProjects/DiningPhilosophers_v2/view/TerminalView.h
